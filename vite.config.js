59c258a8f55d350aad2a7f00cde4f29716a09ef6
@reyeshannahcoleen <reyeshannahcoleen@gmail.comm>
2024-09-02 19:17:09 +0800

    vite.config.js edited

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/reyes_portfolio.git",
   plugins: [react()],
+  base: "/reyes_portfolio/",
 });
